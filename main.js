e597cdeb7088d2ac947508cd1c9a64883e9f8ab9
rafaelkaaaa <147595141+rafaelkaaaa@users.noreply.github.com>
2023-11-21 13:34:18 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function sayHello (name) {
+    console.log ('hi' + name + "!");
+
+}
+sayHello (" Alice");
+function add (a,b) {
+   return a + b;
+
+}
+
+const result = add (3, 5);
+console.log(result)
